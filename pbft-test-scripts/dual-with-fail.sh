2591d9f19173c880f6259af5313ad7faf9a2ec8d
Feng Kaiyu <loveress01@outlook.com>
2022-05-11 14:58:28 +0800

    test: refine test scripts.

--- pbft-test-scripts/dual-with-fail.sh
+++ pbft-test-scripts/dual-with-fail.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+SLEEP=${SLEEP:-30}
 LOG_LEVEL=${RUST_LOG:-afp=trace}
 
-RUST_LOG=$LOG_LEVEL ./target/debug/node-template --alice --tmp --port 30334 2>&1 | tee alice.log &
+RUST_LOG=$LOG_LEVEL ./target/debug/node-template --alice --tmp --node-key 0000000000000000000000000000000000000000000000000000000000000001 --port 30334 2>&1 | tee alice.log &
 THREAD_1=$!
 
 # let alice enter view_change mode.
 sleep 10
 
-RUST_LOG=$LOG_LEVEL ./target/debug/node-template --bob --tmp --bootnodes /ip4/127.0.0.1/tcp/30334/p2p/12D3KooWHnGca3CUcGm5WKsg7b28rv8hJcEb1dwjw5L81XJngNhD 2>&1 | tee bob.log &
+RUST_LOG=$LOG_LEVEL ./target/debug/node-template --bob --tmp --bootnodes /ip4/127.0.0.1/tcp/30334/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp 2>&1 | tee bob.log &
 THREAD_2=$!
 
 # wait for signal, then kill thread
 trap 'kill $THREAD_1 $THREAD_2' SIGINT SIGTERM
 
-wait $THREAD_1 $THREAD_2
+# Exit after sleep 30 seconds.
+sleep "$SLEEP";
+pkill -P $$
