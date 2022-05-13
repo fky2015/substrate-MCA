12b049e4a938d97a10e6658c157e14c3be9a703e
Feng Kaiyu <loveress01@outlook.com>
2022-05-13 23:57:17 +0800

    feat: Add test script for 4 players.

---
+++ pbft-test-scripts/4players-with-view-change-catch-up.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SLEEP=${SLEEP:-30}
+LOG_LEVEL=${LOG_LEVEL:-afp=trace}
+
+# wait for signal, then kill thread
+trap 'pkill -P $$' SIGINT SIGTERM
+
+RUST_LOG=$LOG_LEVEL ./target/debug/node-template --bob --tmp --chain 4players --node-key 0000000000000000000000000000000000000000000000000000000000000002 --port 30334 2>&1 | tee bob.log &
+
+RUST_LOG=$LOG_LEVEL ./target/debug/node-template --charlie --tmp --chain 4players --node-key 0000000000000000000000000000000000000000000000000000000000000003 --bootnodes /ip4/127.0.0.1/tcp/30334/p2p/12D3KooWHdiAxVd8uMQR1hGWXccidmfCwLqcMpGwR6QcTP6QRMuD 2>&1 | tee charlie.log &
+
+RUST_LOG=$LOG_LEVEL ./target/debug/node-template --dave --tmp --chain 4players --node-key 0000000000000000000000000000000000000000000000000000000000000004 --bootnodes /ip4/127.0.0.1/tcp/30334/p2p/12D3KooWHdiAxVd8uMQR1hGWXccidmfCwLqcMpGwR6QcTP6QRMuD 2>&1 | tee dave.log &
+
+sleep 30;
+
+RUST_LOG=$LOG_LEVEL ./target/debug/node-template --alice --tmp --chain 4players --node-key 0000000000000000000000000000000000000000000000000000000000000001 --bootnodes /ip4/127.0.0.1/tcp/30334/p2p/12D3KooWHdiAxVd8uMQR1hGWXccidmfCwLqcMpGwR6QcTP6QRMuD 2>&1 | tee alice.log &
+
+# Exit after sleep 30 seconds.
+sleep "$SLEEP";
+
+pkill -P $$
---
+++ pbft-test-scripts/4players.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SLEEP=${SLEEP:-30}
+LOG_LEVEL=${LOG_LEVEL:-afp=trace}
+
+# wait for signal, then kill thread
+trap 'pkill -P $$' SIGINT SIGTERM
+
+RUST_LOG=$LOG_LEVEL ./target/debug/node-template --alice --tmp --chain 4players --node-key 0000000000000000000000000000000000000000000000000000000000000001 --port 30334 2>&1 | tee alice.log &
+
+RUST_LOG=$LOG_LEVEL ./target/debug/node-template --bob --tmp --chain 4players --node-key 0000000000000000000000000000000000000000000000000000000000000002 --bootnodes /ip4/127.0.0.1/tcp/30334/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp 2>&1 | tee bob.log &
+
+RUST_LOG=$LOG_LEVEL ./target/debug/node-template --charlie --tmp --chain 4players --node-key 0000000000000000000000000000000000000000000000000000000000000003 --bootnodes /ip4/127.0.0.1/tcp/30334/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp 2>&1 | tee charlie.log &
+
+RUST_LOG=$LOG_LEVEL ./target/debug/node-template --dave --tmp --chain 4players --node-key 0000000000000000000000000000000000000000000000000000000000000004 --bootnodes /ip4/127.0.0.1/tcp/30334/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp 2>&1 | tee dave.log &
+
+# Exit after sleep 30 seconds.
+sleep "$SLEEP";
+pkill -P $$
