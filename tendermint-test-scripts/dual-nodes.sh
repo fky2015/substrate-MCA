d83edc5a79f75ef7c1a8d7ad51d003bb88f9b895
Feng Kaiyu <loveress01@outlook.com>
2022-09-19 10:36:29 +0800

    test: add test scripts for node-tendermint.

---
+++ tendermint-test-scripts/dual-nodes.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SLEEP=${SLEEP:-30}
+LOG_LEVEL=${LOG_LEVEL:-afp=info}
+
+RUST_LOG=$LOG_LEVEL ./target/debug/node-tendermint --alice --tmp --node-key 0000000000000000000000000000000000000000000000000000000000000001 --port 30334 2>&1 | tee alice.log &
+THREAD_1=$!
+
+RUST_LOG=$LOG_LEVEL ./target/debug/node-tendermint --bob --tmp --bootnodes /ip4/127.0.0.1/tcp/30334/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp 2>&1 | tee bob.log &
+THREAD_2=$!
+
+# wait for signal, then kill thread
+trap 'kill $THREAD_1 $THREAD_2' SIGINT SIGTERM
+
+# Exit after sleep 30 seconds.
+sleep "$SLEEP";
+pkill -P $$
